eb1dec472d35717a440ce6b1579a6e305ccf1052
Saso Miocinovic <saso.miocinovic@gmail.com>
2016-06-06 19:48:52 +0200

    Added some data annotations to model and changed input type for Content in a view.

--- MvcCMS/Models/Post.cs
+++ MvcCMS/Models/Post.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,11 +9,16 @@ namespace MvcCMS.Models
     public class Post
     {
         //ID of post will be represented by unique URL, thus string
+        [Display(Name = "Slug")]
         public string Id { get; set; }
+        [Display(Name = "Title")]
         public string Title { get; set; }
+        [Display(Name = "Post Content")]
         public string Content { get; set; }
+        [Display(Name = "Date Created")]
         public DateTime Created { get; set; }
         //Nullable - so that if it's null it hasn't been published yet
+        [Display(Name = "Date Published")]
         public DateTime? Published { get; set; }
         public int AuthorId { get; set; }
     }
