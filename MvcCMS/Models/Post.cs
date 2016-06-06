5e44473e4b94cbf8de17564522880f741ea72c32
Saso Miocinovic <saso.miocinovic@gmail.com>
2016-06-06 10:39:43 +0200

    Added model to represent our posts.

---
+++ MvcCMS/Models/Post.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcCMS.Models
+{
+    public class Post
+    {
+        //ID of post will be represented by unique URL, thus string
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime Created { get; set; }
+        //Nullable - so that if it's null it hasn't been published yet
+        public DateTime? Published { get; set; }
+        public int AuthorId { get; set; }
+    }
+}
